5a1128a9dd85ad59081fefa01752c2d7fa8370ed
Jonathan Gao <gaolei33@gmail.com>
2013-11-22 00:34:37 +0800

    network IP retrieving method modified for Linux

--- ptp_automate_tool/settings.py
+++ ptp_automate_tool/settings.py
@@ -28,14 +28,15 @@ DATABASES = {
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 
-#def getNetworkIp():
-#    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#    s.connect(('google.com', 80))
-#    return s.getsockname()[0]
-
 
 def get_network_ip():
-    return socket.gethostbyname(socket.gethostname())
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('google.com', 80))
+    return s.getsockname()[0]
+
+
+#def get_network_ip():
+#    return socket.gethostbyname(socket.gethostname())
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', get_network_ip()]
 
