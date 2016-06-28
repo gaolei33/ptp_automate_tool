a7d8bbaf697351ba954e6866a9799dfe1e6f8a41
Jonathan Gao <gaolei33@gmail.com>
2016-06-28 15:49:19 +0800

    - Upgraded to Django 1.9.7 - Fixed warnings of deprecated functions in latest Django - Upgraded uWSGI to latest version - Use tag 'static' instead of hard-coded path in templates

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,85 +1,85 @@
 from django.views.generic.base import RedirectView
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from webapp.view import db_view
+from webapp.view import csv_view
+from webapp.view import bus_stop_view
+from webapp.view import bus_service_view
+from webapp.view import bus_route_view
+from webapp.view import address_view
+from webapp.view import pdf_view
+from webapp.view import sql_view
+from webapp.view import street_view
+from webapp.view import html_shp_kml_view
 
 __author__ = 'Gao Lei'
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/webapp/db/')),
-)
+]
 
-urlpatterns += patterns(
-    'webapp.view.db_view',
+urlpatterns += [
     url(r'^db/$', RedirectView.as_view(url='/webapp/db/db_backup/')),
-    url(r'^db/db_backup/$', 'db_home', {'method': 'DB_BACKUP', 'description': 'DB Backup'}),
-    url(r'^db/db_restore/$', 'db_home', {'method': 'DB_RESTORE', 'description': 'DB Restore'}),
-    url(r'^db/db_backup_delete/$', 'db_home', {'method': 'DB_BACKUP_DELETE', 'description': 'DB Backup Delete'}),
-    url(r'^db/db_handler/$', 'db_handler'),
-)
+    url(r'^db/db_backup/$', db_view.db_home, {'method': 'DB_BACKUP', 'description': 'DB Backup'}),
+    url(r'^db/db_restore/$', db_view.db_home, {'method': 'DB_RESTORE', 'description': 'DB Restore'}),
+    url(r'^db/db_backup_delete/$', db_view.db_home, {'method': 'DB_BACKUP_DELETE', 'description': 'DB Backup Delete'}),
+    url(r'^db/db_handler/$', db_view.db_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.csv_view',
+urlpatterns += [
     url(r'^csv/$', RedirectView.as_view(url='/webapp/csv/csv_upload/')),
-    url(r'^csv/csv_upload/$', 'csv_home', {'method': 'CSV_UPLOAD', 'description': 'CSV Upload'}),
-    url(r'^csv/csv_delete/$', 'csv_home', {'method': 'CSV_DELETE', 'description': 'CSV Delete'}),
-    url(r'^csv/csv_handler/$', 'csv_handler'),
-)
+    url(r'^csv/csv_upload/$', csv_view.csv_home, {'method': 'CSV_UPLOAD', 'description': 'CSV Upload'}),
+    url(r'^csv/csv_delete/$', csv_view.csv_home, {'method': 'CSV_DELETE', 'description': 'CSV Delete'}),
+    url(r'^csv/csv_handler/$', csv_view.csv_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.bus_stop_view',
+urlpatterns += [
     url(r'^bus_stop/$', RedirectView.as_view(url='/webapp/bus_stop/bus_stop_add/')),
-    url(r'^bus_stop/bus_stop_add/$', 'bus_stop_home', {'method': 'BUS_STOP_ADD', 'description': 'Bus Stop Add'}),
-    url(r'^bus_stop/bus_stop_update/$', 'bus_stop_home', {'method': 'BUS_STOP_UPDATE', 'description': 'Bus Stop Update'}),
-    url(r'^bus_stop/bus_stop_detail/$', 'bus_stop_detail'),
-    url(r'^bus_stop/bus_stop_handler/$', 'bus_stop_handler'),
-)
+    url(r'^bus_stop/bus_stop_add/$', bus_stop_view.bus_stop_home, {'method': 'BUS_STOP_ADD', 'description': 'Bus Stop Add'}),
+    url(r'^bus_stop/bus_stop_update/$', bus_stop_view.bus_stop_home, {'method': 'BUS_STOP_UPDATE', 'description': 'Bus Stop Update'}),
+    url(r'^bus_stop/bus_stop_detail/$', bus_stop_view.bus_stop_detail),
+    url(r'^bus_stop/bus_stop_handler/$', bus_stop_view.bus_stop_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.bus_service_view',
+urlpatterns += [
     url(r'^bus_service/$', RedirectView.as_view(url='/webapp/bus_service/bus_service_add_or_update/')),
-    url(r'^bus_service/bus_service_add_or_update/$', 'bus_service_home', {'method': 'BUS_SERVICE_ADD_OR_UPDATE', 'description': 'Bus Service Add / Update'}),
-    url(r'^bus_service/bus_service_enable_or_disable/$', 'bus_service_home', {'method': 'BUS_SERVICE_ENABLE_OR_DISABLE', 'description': 'Bus Service Enable / Disable'}),
-    url(r'^bus_service/bus_service_handler/$', 'bus_service_handler'),
-)
+    url(r'^bus_service/bus_service_add_or_update/$', bus_service_view.bus_service_home, {'method': 'BUS_SERVICE_ADD_OR_UPDATE', 'description': 'Bus Service Add / Update'}),
+    url(r'^bus_service/bus_service_enable_or_disable/$', bus_service_view.bus_service_home, {'method': 'BUS_SERVICE_ENABLE_OR_DISABLE', 'description': 'Bus Service Enable / Disable'}),
+    url(r'^bus_service/bus_service_handler/$', bus_service_view.bus_service_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.bus_route_view',
+urlpatterns += [
     url(r'^bus_route/$', RedirectView.as_view(url='/webapp/bus_route/bus_route_add_or_update/')),
-    url(r'^bus_route/bus_route_add_or_update/$', 'bus_route_home', {'method': 'BUS_ROUTE_ADD_OR_UPDATE', 'description': 'Bus Route Add / Update'}),
-    url(r'^bus_route/bus_route_handler/$', 'bus_route_handler'),
-)
+    url(r'^bus_route/bus_route_add_or_update/$', bus_route_view.bus_route_home, {'method': 'BUS_ROUTE_ADD_OR_UPDATE', 'description': 'Bus Route Add / Update'}),
+    url(r'^bus_route/bus_route_handler/$', bus_route_view.bus_route_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.address_view',
+urlpatterns += [
     url(r'^address/$', RedirectView.as_view(url='/webapp/address/address_add/')),
-    url(r'^address/address_add/$', 'address_home', {'method': 'ADDRESS_ADD', 'description': 'Address Add'}),
-    url(r'^address/address_detail/$', 'address_detail'),
-    url(r'^address/address_handler/$', 'address_handler'),
-)
+    url(r'^address/address_add/$', address_view.address_home, {'method': 'ADDRESS_ADD', 'description': 'Address Add'}),
+    url(r'^address/address_detail/$', address_view.address_detail),
+    url(r'^address/address_handler/$', address_view.address_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.sql_view',
+urlpatterns += [
     url(r'^sql/$', RedirectView.as_view(url='/webapp/sql/sql_download/')),
-    url(r'^sql/sql_download/$', 'sql_home', {'method': 'SQL_DOWNLOAD', 'description': 'SQL Download'}),
-    url(r'^sql/sql_merged_download/$', 'sql_home', {'method': 'SQL_MERGED_DOWNLOAD', 'description': 'SQL Merged Download'}),
-    url(r'^sql/sql_delete/$', 'sql_home', {'method': 'SQL_DELETE', 'description': 'SQL Delete'}),
-    url(r'^sql/sql_handler/$', 'sql_handler'),
-)
+    url(r'^sql/sql_download/$', sql_view.sql_home, {'method': 'SQL_DOWNLOAD', 'description': 'SQL Download'}),
+    url(r'^sql/sql_merged_download/$', sql_view.sql_home, {'method': 'SQL_MERGED_DOWNLOAD', 'description': 'SQL Merged Download'}),
+    url(r'^sql/sql_delete/$', sql_view.sql_home, {'method': 'SQL_DELETE', 'description': 'SQL Delete'}),
+    url(r'^sql/sql_handler/$', sql_view.sql_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.pdf_view',
+urlpatterns += [
     url(r'^pdf/$', RedirectView.as_view(url='/webapp/pdf/pdf_rename/')),
-    url(r'^pdf/pdf_rename/$', 'pdf_home', {'method': 'PDF_RENAME', 'description': 'PDF Rename'}),
-    url(r'^pdf/pdf_handler/$', 'pdf_handler'),
-)
+    url(r'^pdf/pdf_rename/$', pdf_view.pdf_home, {'method': 'PDF_RENAME', 'description': 'PDF Rename'}),
+    url(r'^pdf/pdf_handler/$', pdf_view.pdf_handler),
+]
 
-urlpatterns += patterns(
-    'webapp.view.street_view',
-    url(r'^street/street_search/$', 'street_search'),
-)
+urlpatterns += [
+    url(r'^street/street_search/$', street_view.street_search),
+]
 
-urlpatterns += patterns(
-    'webapp.view.html_shp_kml_view',
-    url(r'^html_shp_kml/$', 'html_shp_kml_home', {'description': 'HTML & SHP & KML'}),
-)
+urlpatterns += [
+    url(r'^html_shp_kml/$', html_shp_kml_view.html_shp_kml_home, {'description': 'HTML & SHP & KML'}),
+]
